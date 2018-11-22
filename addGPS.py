d6e2526ad5afaa70b47d507814d87d0a8964a762
morgon <morgon08@mail.ru>
2018-11-22 20:59:52 +0700

    добавлина возможность добовлять высоту равная float

--- addGPS.py
+++ addGPS.py
@@ -2,12 +2,13 @@ from GPSPhoto import gpsphoto
 
 # Create a GPSPhoto Object
 photo = gpsphoto.GPSPhoto()
-photo = gpsphoto.GPSPhoto(r"C:\Users\admin\EXIFparsing\pexif\test\data\noexif.jpg")
+pachPhoto  = r"C:\Users\admin\PycharmProjects\untitled3\1.jpg"
+photo = gpsphoto.GPSPhoto(pachPhoto)
 
 # Create GPSInfo Data Object
 
-info = gpsphoto.GPSInfo((35.104860, -106.628915), alt=10)
+info = gpsphoto.GPSInfo((35.104860, -106.628915), alt=3925203)  #3925203000 392.5203
 
 # Modify GPS Data
-photo.modGPSData(info, r"C:\Users\admin\EXIFparsing\pexif\test\data\noexif.jpg")
+photo.modGPSData(info, pachPhoto)
 
--- run.py
+++ run.py
@@ -4,15 +4,15 @@ from GPSPhoto import gpsphoto
 
 if __name__ == "__main__":
 
-    pachCSV = r'C:\Users\admin\PycharmProjects\untitled3\Исходники1\1.xls'
+    pachCSV = r'H:\фото ташт\1.xls'
 
     rb = xlrd.open_workbook(pachCSV, formatting_info=True)
     sheet = rb.sheet_by_index(0)
     val = sheet.row_values(0)[0]
     vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
     photo = gpsphoto.GPSPhoto()
-    pach_photo = r'C:\Users\admin\PycharmProjects\untitled3\Исходники1'
-    pach_itog = r'C:\Users\admin\PycharmProjects\untitled3\Исходники1\itog'
+    pach_photo = r'H:\фото ташт\ташт фото'
+    pach_itog = r'H:\фото ташт\itog'
 
     for i in vals:
         name = i[0]
@@ -20,13 +20,13 @@ if __name__ == "__main__":
         longitude = i[2] #Долгота
         height = i[3] #высота
 
-        pach_photo1 = pach_photo + "\\" + name
-        pach_itog1 = pach_itog + "\\" + name
+        pach_photo1 = pach_photo + "\\" + name + '.JPG'
+        pach_itog1 = pach_itog + "\\" + name + '.JPG'
         print(pach_photo)
         print(pach_itog)
         try:
             photo = gpsphoto.GPSPhoto(pach_photo1)
-            info = gpsphoto.GPSInfo((float(latitude), float(longitude)), alt=round(float(height)))
+            info = gpsphoto.GPSInfo((float(latitude), float(longitude)), alt=int(height * 10000))
             photo.modGPSData(info, pach_itog1)
         except FileNotFoundError:
             print("error")
